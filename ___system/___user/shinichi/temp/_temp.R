3b807e4b81e82e7b8781d5bd740cebc6aad79f2a
Shinichi Yamada <shyama818@gmail.com>
2021-07-03 00:38:59 +1200

    update: rlib

--- ___system/___user/shinichi/temp/_temp.R
+++ ___system/___user/shinichi/temp/_temp.R
@@ -1,4 +1,4 @@
-.libPaths("/home/shinichi/workspace/___system/Rlib")
+.libPaths("/home/shinichi/workspace/lalab/___system/rlib")
 if (!require("readr")) install.packages("readr")
 if (!require("tidyverse")) install.packages("tidyverse")
 if (!require("ggplot2")) install.packages("ggplot2")
@@ -9,9 +9,10 @@ if (!require("haven")) install.packages("haven")
 if (!require("rmatio")) install.packages("rmatio")
 if (!require("foreign")) install.packages("foreign")
 if (!require("chron")) install.packages("chron")
-if (!require("papeR")) install.packages("papeR")
+if (!require("plyr")) install.packages("plyr")
 if (!require("car")) install.packages("car")
 if (!require("qqplotr")) install.packages("qqplotr")
+if (!require("R.matlab")) install.packages("R.matlab")
 
 library(readr)
 library(tidyverse)
@@ -24,18 +25,14 @@ library(rmatio)
 library(foreign)
 library(chron)
 library(plyr)
-library(papeR)
 library(car)
 library(qqplotr)
-
 library(devtools)
 library(roxygen2)
 library(R.matlab)
-library(pracma)
 library(matrixStats)
 library(MASS)
 library(R.utils)
-library(microbenchmark)
 library(Rcpp)
 library(RcppArmadillo)
 library(lar)
@@ -43,27 +40,21 @@ library(larcpp)
 
 
 
-adj <- readRDS("/home/shinichi/workspace/test/Dataset/___datasets/adj/___data/adj.rds")
-anti <- readRDS("/home/shinichi/workspace/test/Dataset/___datasets/anti/___data/anti.rds")
-bld <- readRDS("/home/shinichi/workspace/test/Dataset/___datasets/bld/___data/bld.rds")
-bldef <- readRDS("/home/shinichi/workspace/test/Dataset/___datasets/bldef/___data/bldef.rds")
-demog <- readRDS("/home/shinichi/workspace/test/Dataset/___datasets/demog/___data/demog.rds")
+adj <- read.csv("/home/shinichi/Eclipse/data/data/adj.csv")
+save_data(adj,"adj","/home/shinichi/workspace/lalab/___project/test/Datasource/___datasets/adj")
 
-# Creation of ML Data 
-demog___ml1 <- demog[,c("BMI","BSHR","BSSYS","BSDIA","BLWT","AGECAT","WT1CLS","WT2CLS","TRTGRP","SEX","ETHNIC")] 
-# Handling of Missing Data 
-demog___ml1 <- miss_main(demog___ml1, c("BSHR","BSSYS","BSDIA","BLWT","AGECAT","WT1CLS","WT2CLS","TRTGRP","SEX","ETHNIC"), c("BMI"), c(1,1,1,1,2,2,2,3,3,3), 0) 
-save_data(demog___ml1,"demog___ml1","/home/shinichi/workspace/test/Tree/test/TRTGRP/AGECAT/SEX/ETHNIC/BMI/ml1/___datasets/demog___ml1")
+anti <- read.csv("/home/shinichi/Eclipse/data/data/anti.csv")
+save_data(anti,"anti","/home/shinichi/workspace/lalab/___project/test/Datasource/___datasets/anti")
 
-output <- ta_strat(demog___ml1,NULL,c("demog___ml1","test_error","prediction","feature_rank"), .fun = function(xx) {
-ml_main(xx, c("BSHR","BSSYS","BSDIA","BLWT","AGECAT","WT1CLS","WT2CLS","TRTGRP","SEX","ETHNIC"), c("BMI"), c(1,1,1,1,2,2,2,3,3,3), 4, 0, 70, 30, 0, 1e-5, 3, 3, 2) 
-})
-save_data(output$test_error,"test_error","/home/shinichi/workspace/test/Tree/test/TRTGRP/AGECAT/SEX/ETHNIC/BMI/ml1/___datasets/test_error")
+bld <- read.csv("/home/shinichi/Eclipse/data/data/bld.csv")
+save_data(bld,"bld","/home/shinichi/workspace/lalab/___project/test/Datasource/___datasets/bld")
 
-save_data(output$prediction,"prediction","/home/shinichi/workspace/test/Tree/test/TRTGRP/AGECAT/SEX/ETHNIC/BMI/ml1/___datasets/prediction")
+bldef <- read.csv("/home/shinichi/Eclipse/data/data/bldef.csv")
+save_data(bldef,"bldef","/home/shinichi/workspace/lalab/___project/test/Datasource/___datasets/bldef")
 
-save_data(output$feature_rank,"feature_rank","/home/shinichi/workspace/test/Tree/test/TRTGRP/AGECAT/SEX/ETHNIC/BMI/ml1/___datasets/feature_rank")
+demog <- read.csv("/home/shinichi/Eclipse/data/data/demog.csv")
+save_data(demog,"demog","/home/shinichi/workspace/lalab/___project/test/Datasource/___datasets/demog")
 
-saveRDS(c(1), file="/home/shinichi/workspace/___system/___user/shyama818/end/_end.R")
+saveRDS(c(1), file="/home/shinichi/workspace/lalab/___system/___user/shinichi/end/_end.R")
 
 
