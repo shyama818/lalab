e1994a583178099d2d3229b687ea1e1203709a76
shyama818 <47661979+shyama818@users.noreply.github.com>
2021-07-26 18:14:00 +1200

    Delete ___project/test directory

--- ___project/test/Dataset/___datasets/demog/___variables/SEX/___code/SEX.R
+++
@@ -1,4 +0,0 @@
-
-demog$SEX <- factor(demog$SEX, levels = c("Female", "Male"), ordered = FALSE)
-save_data(demog,"demog","/home/shinichi/workspaceRemote/lalab/___project/test/Dataset/___datasets/demog")
-
